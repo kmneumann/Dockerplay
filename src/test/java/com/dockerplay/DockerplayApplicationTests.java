ea035f4b21e8f01d10e6fa4fe8ed47ab735afc11
Kajetan Neumann <k.m.neumann@student.tudelft.nl>
2024-05-09 16:04:15 +0200

    First commit

---
+++ src/test/java/com/dockerplay/DockerplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dockerplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
